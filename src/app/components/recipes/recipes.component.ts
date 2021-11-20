74783247348ab5084c91c50bc242c5ba87e44187
Salima Mukhit <salima@consilientlabs.com>
2021-11-20 15:08:47 -0800

    parses results correctly

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -12,10 +12,15 @@ export class RecipesComponent implements OnInit {
 
   allIngredients: any;
 
+  recipeList: any;
+
+  done: boolean | undefined;
+
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.ingredientToAdd = '';
+    this.done = false;
     this.getAllIngredients();
   }
 
@@ -41,7 +46,35 @@ export class RecipesComponent implements OnInit {
     let query = this.prepareQuery();
     this.recipeService.getRecipeList(query).then((result: any) => {
       console.log(result);
+      this.sortResult(result);
     });
+  }
+
+  sortResult(result: any): void {
+    this.recipeList = [];
+    let resultMap = new Map<string, number>();
+
+    for(let entry of result) {
+      if(resultMap.has(entry['url'])) {
+        let count = resultMap.get(entry['url']);
+        if(count) {
+          resultMap.set(entry['url'], count + 1);
+        }
+      } else {
+        resultMap.set(entry['url'], 1);
+      }
+    }
+
+    for(let key of resultMap.keys()) {
+      let recipeName = result.find((entry: any) => entry['url'] == key)['name'];
+      let item = { 'name': recipeName, 'url': key, 'count': resultMap.get(key) };
+      this.recipeList.push(item);
+    }
+
+    this.recipeList.sort((a: any, b: any) => {return b['count'] - a['count']});
+
+    this.done = true;
+
   }
 
   prepareQuery(): string {
