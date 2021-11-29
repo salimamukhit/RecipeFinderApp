4574717286b231425ee27f380502b5c3f2fb15db
Salima Mukhit <smukhit@dons.usfca.edu>
2021-11-28 18:25:44 -0800

    changed prod.ts url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  api_url: '{HTTP_PROTOCOL}://{WEBAPP_HOST_IP}:{WEBAPP_PORT}',
+  api_url: '{HTTP_PROTOCOL}://{WEBAPP_HOST_IP}:{WEBAPP_PORT}/api/v1',
   production: true
 };
